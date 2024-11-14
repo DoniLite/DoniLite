d5f61c529cf00b346f4aeed881f1eefc1a2d472f
DoniLite <yaomessan13@outlook.com>
2024-11-14 10:59:48 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/', // Remplacez par le nom de votre dépôt
+  base: '/DoniLite/', // Remplacez par le nom de votre dépôt
   // ... autres configurations
 });
