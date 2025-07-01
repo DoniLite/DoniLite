f9b6c6bf509f71ad3c445fc787847651098abf8c
DoniLite <yaomessan13@outlook.com>
2025-07-01 23:32:41 +0000

    feat: add admin login page and calendar components with Sonner integration

--- pages/dashboard/login.vue
+++ pages/dashboard/login.vue
@@ -1,3 +1,54 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+})
+</script>
+
 <template>
-    Login Page
+  <div class="flex min-h-screen w-full items-center justify-center">
+    <div class="bg-card min-w-xl rounded-lg p-8 shadow-lg">
+      <h1 class="text-5xl font-bold md:text-xl">{{ $t('admin.login.welcome') }}</h1>
+      <span>{{ $t('admin.login.description') }}</span>
+      <form
+        class="mt-8 flex flex-col gap-4"
+        action=""
+      >
+        <div class="flex w-full flex-col gap-2">
+          <label
+            for="login"
+            class="font-bold"
+          >
+            {{ $t('admin.login.input.login:input.label') }}
+          </label>
+          <input
+            type="text"
+            :placeholder="$t('admin.login.input.login:input.placeholder')"
+            name="login"
+            class="ring-muted focus:ring-primary rounded-md bg-transparent px-4 py-2 ring outline-none"
+          />
+        </div>
+
+        <div class="flex w-full flex-col gap-2">
+          <label
+            for="password"
+            class="font-bold"
+          >
+            {{ $t('admin.login.input.password:input.label') }}
+          </label>
+          <input
+            type="password"
+            :placeholder="$t('admin.login.input.password:input.placeholder')"
+            name="password"
+            class="ring-muted focus:ring-primary rounded-md bg-transparent px-4 py-2 ring outline-none"
+          />
+        </div>
+
+        <Button
+          class="bg-primary text-primary-foreground w-full cursor-pointer rounded-md px-4 py-2 font-bold"
+        >
+          {{ $t('admin.login.input.submit') }}
+        </Button>
+      </form>
+    </div>
+  </div>
 </template>
